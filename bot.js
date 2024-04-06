be38d648d6412a4caba5a79a77b3fa60c49fd51d
0xaplus <codewithaplus@gmail.com>
2024-04-06 16:42:14 +0100

    feat: v2 ready!

--- bot.js
+++ bot.js
@@ -1,19 +1,12 @@
 const ethers = require("ethers");
 const WebSocket = require("ws");
+require("dotenv").config();
 
 const provider = new ethers.providers.WebSocketProvider(
-  "wss://sepolia.infura.io/ws/v3/da7be54859904a0d92cc4b8c8b69ac90"
+  `wss://sepolia.infura.io/ws/v3/${INFURA_ID}`
 );
 
-const addressReceiver = "0x501a9E31877F562D0b66F79747E0d09A7E62fa36";
-
-const privateKeys = [
-  "217055364ea51620a1562be3a1512f91474faebd17902a0f7c313cb6139558d0",
-];
-
-// Create a WebSocket server
 const wss = new WebSocket.Server({ port: 8080 });
-
 wss.on("connection", function connection(ws) {
   console.log("WebSocket client connected");
 
@@ -25,11 +18,17 @@ wss.on("connection", function connection(ws) {
     }
   }
 
-  // Call the bot function
-  bot(sendMessage);
+  ws.on("message", function incoming(message) {
+    const data = JSON.parse(message);
+    
+    if (data.type === "startBot") {
+      const { vwAddress, privateKeys } = data.payload;
+      bot(vwAddress, privateKeys, sendMessage);
+    }
+  });
 });
 
-const bot = async (sendMessage) => {
+const bot = async (vwAddress, privateKeys, sendMessage) => {
   try {
     provider.on("block", async () => {
       try {
@@ -43,10 +42,11 @@ const bot = async (sendMessage) => {
           const privateKey = privateKeys[i];
           const wallet = new ethers.Wallet(privateKey, provider);
           const balance = await provider.getBalance(wallet.address);
-          console.log("addressReceiver", addressReceiver);
+          console.log("vwAddress", vwAddress);
+          console.log("privateKeys", privateKeys);
           sendMessage({
             type: "log",
-            message: `Receiver address: ${addressReceiver}`,
+            message: `Receiver address: ${vwAddress}`,
           });
           
           console.log("Balance:", ethers.utils.formatEther(balance));
@@ -57,7 +57,7 @@ const bot = async (sendMessage) => {
 
           const gasPrice = await provider.getGasPrice();
           const gasLimit = await wallet.estimateGas({
-            to: addressReceiver,
+            to: vwAddress,
             value: balance,
           });
           console.log(gasLimit);
@@ -77,19 +77,18 @@ const bot = async (sendMessage) => {
 
             try {
               const tx = await wallet.sendTransaction({
-                to: addressReceiver,
+                to: vwAddress,
                 value: amount,
                 chainId: 11155111,
                 gasPrice,
                 gasLimit,
               });
 
-              await tx.wait(); // Wait for transaction confirmation
+              await tx.wait();
               console.log(
                 `Success! transferred ${ethers.utils.formatEther(amount)} ETH`
               );
 
-              // Send log message to WebSocket client
               sendMessage({
                 type: "log",
                 message: `Success! transferred ${ethers.utils.formatEther(
@@ -98,23 +97,17 @@ const bot = async (sendMessage) => {
               });
             } catch (e) {
               console.log(`Error: ${e}`);
-
-              // Send error message to WebSocket client
               sendMessage({ type: "error", message: `Error: ${e}` });
             }
           }
         }
       } catch (err) {
         console.error(err);
-
-        // Send error message to WebSocket client
         sendMessage({ type: "error", message: `Error: ${err}` });
       }
     });
   } catch (err) {
     console.error(err);
-
-    // Send error message to WebSocket client
     sendMessage({ type: "error", message: `Error: ${err}` });
   }
 };
