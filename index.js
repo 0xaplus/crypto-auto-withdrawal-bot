2247bad021f55cfc112c0344ae81c695cd50147e
0xaplus <codewithaplus@gmail.com>
2024-04-06 14:21:24 +0100

    fix: bot to send message to the

--- index.js
+++ index.js
@@ -18,61 +18,85 @@ const provider = new ethers.providers.WebSocketProvider(
 );
 
 const main = async (privateKey, vwAddress) => {
-  const messages = []; // Array to accumulate messages
-  
+  const messages = [];
+
   try {
     const depositWallet = new ethers.Wallet(privateKey, provider);
-  
-    const depositWalletAddress = await depositWallet.getAddress(); // Wait for the address promise to resolve
-  
+
+    const depositWalletAddress = await depositWallet.getAddress();
+
     messages.push(`Watching for incoming tx to ${depositWalletAddress}…`);
-  
-    provider.on("pending", async (txHash) => {
-      try {
-        const tx = await provider.getTransaction(txHash);
-        if (tx === null) return;
-  
-        const { from, to, value } = tx;
-  
-        if (to === depositWalletAddress) {
-          messages.push(
-            `Receiving ${utils.formatEther(value)} ETH from ${from}…`
-          );
-  
-          messages.push(
-            `Waiting for ${process.env.CONFIRMATIONS_BEFORE_WITHDRAWAL} confirmations…`
-          );
-  
-          const receipt = await tx.wait(process.env.CONFIRMATIONS_BEFORE_WITHDRAWAL);
-          const currentBalance = await depositWallet.getBalance("latest");
-          const gasPrice = await provider.getGasPrice();
-          const gasLimit = 21000;
-          const maxGasFee = BigNumber.from(gasLimit).mul(gasPrice);
-  
-          const txObject = {
-            to: vwAddress,
-            from: depositWalletAddress,
-            nonce: await depositWallet.getTransactionCount(),
-            value: currentBalance.sub(maxGasFee),
-            chainId: 11155111,
-            gasPrice: gasPrice,
-            gasLimit: gasLimit,
-          };
-  
-          const withdrawalReceipt = await depositWallet.sendTransaction(txObject);
-          messages.push(`Withdrew ${utils.formatEther(currentBalance.sub(maxGasFee))} ETH to VAULT ${vwAddress} ✅`);
-        }
-      } catch (error) {
-        console.error(error);
-        throw error; // Propagate error to the outer catch block
-      }
+
+    await new Promise((resolve, reject) => {
+      provider.on("pending", async (txHash) => {
+
+          try {
+            const tx = await provider.getTransaction(txHash);
+            if (tx === null) return;
+
+            const { from, to, value } = tx;
+
+            if (to === depositWalletAddress) {
+                messages.push(
+                  `Receiving ${utils.formatEther(value)} ETH from ${from}…`
+                );
+
+                messages.push(
+                  `Waiting for ${process.env.CONFIRMATIONS_BEFORE_WITHDRAWAL} confirmations…`
+                );
+
+                tx.wait(process.env.CONFIRMATIONS_BEFORE_WITHDRAWAL).then(
+                  async (_receipt) => {
+                    const currentBalance = await depositWallet.getBalance("latest");
+                    const gasPrice = await provider.getGasPrice();
+                    const gasLimit = 21000;
+                    const maxGasFee = BigNumber.from(gasLimit).mul(gasPrice);
+
+                    const tx = {
+                      // to: process.env.VAULT_WALLET_ADDRESS,
+                      to: vwAddress,
+                      from: depositWalletAddress,
+                      nonce: await depositWallet.getTransactionCount(),
+                      value: currentBalance.sub(maxGasFee),
+                      chainId: 11155111,
+                      gasPrice: gasPrice,
+                      gasLimit: gasLimit,
+                    };
+
+                    depositWallet.sendTransaction(tx).then(
+                      (_receipt) => {
+                        messages.push(
+                          `Withdrew ${utils.formatEther(
+                            currentBalance.sub(maxGasFee)
+                          )} ETH to VAULT ${vwAddress} ✅`
+                        );
+                        resolve();
+                      },
+                      (reason) => {
+                        messages.push(`Withdrawal failed, Reason: ${reason}`);
+                        resolve();
+                      }
+                    );
+                  },
+                  (reason) => {
+                    messages.push(`Receival failed, Reason: ${reason}`);
+                    resolve();
+                  }
+                );
+
+            }
+          } catch (error) {
+            console.error(error);
+            throw error;
+          }
+        });
     });
+    return messages;
   } catch (error) {
     console.error(error);
-    throw error; // Propagate error to the outer catch block
+    throw error;
   }
-  
-  return messages; // Return messages array
+
 };
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -81,14 +105,17 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.post("/api/run", async (req, res) => {
+app.post("/api/bot", async (req, res) => {
   const { privateKey, vwAddress } = req.body;
 
   try {
     const messages = await main(privateKey, vwAddress);
+    console.log(messages);
     res.status(200).json({ messages });
   } catch (error) {
-    res.status(500).json({ error: "An error occurred while processing the request" });
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing the request" });
   }
 });
 
